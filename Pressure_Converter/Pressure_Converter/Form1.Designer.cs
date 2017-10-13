a629f74a85d99cc3942524dfaa7706908de282b7
wink94 <mwindula16@gmail.com>
2017-10-13 19:04:06 +0530

    first commit

---
+++ Pressure_Converter/Pressure_Converter/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace Pressure_Converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblEntry = new System.Windows.Forms.Label();
+            this.lblOut = new System.Windows.Forms.Label();
+            this.lblVal = new System.Windows.Forms.Label();
+            this.lblOutputAnswer = new System.Windows.Forms.Label();
+            this.cmbEntry = new System.Windows.Forms.ComboBox();
+            this.cmbOut = new System.Windows.Forms.ComboBox();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.lblDisplay = new System.Windows.Forms.Label();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblEntry
+            // 
+            this.lblEntry.AutoSize = true;
+            this.lblEntry.Location = new System.Drawing.Point(103, 65);
+            this.lblEntry.Name = "lblEntry";
+            this.lblEntry.Size = new System.Drawing.Size(53, 13);
+            this.lblEntry.TabIndex = 0;
+            this.lblEntry.Text = "Entry Unit";
+            // 
+            // lblOut
+            // 
+            this.lblOut.AutoSize = true;
+            this.lblOut.Location = new System.Drawing.Point(345, 65);
+            this.lblOut.Name = "lblOut";
+            this.lblOut.Size = new System.Drawing.Size(61, 13);
+            this.lblOut.TabIndex = 1;
+            this.lblOut.Text = "Output Unit";
+            // 
+            // lblVal
+            // 
+            this.lblVal.AutoSize = true;
+            this.lblVal.Location = new System.Drawing.Point(103, 151);
+            this.lblVal.Name = "lblVal";
+            this.lblVal.Size = new System.Drawing.Size(62, 13);
+            this.lblVal.TabIndex = 2;
+            this.lblVal.Text = "Enter Value";
+            // 
+            // lblOutputAnswer
+            // 
+            this.lblOutputAnswer.AutoSize = true;
+            this.lblOutputAnswer.Location = new System.Drawing.Point(103, 208);
+            this.lblOutputAnswer.Name = "lblOutputAnswer";
+            this.lblOutputAnswer.Size = new System.Drawing.Size(39, 13);
+            this.lblOutputAnswer.TabIndex = 3;
+            this.lblOutputAnswer.Text = "Output";
+            // 
+            // cmbEntry
+            // 
+            this.cmbEntry.FormattingEnabled = true;
+            this.cmbEntry.Items.AddRange(new object[] {
+            "Pascal",
+            "atm",
+            "mmHg",
+            "bar",
+            "torr",
+            "psi"});
+            this.cmbEntry.Location = new System.Drawing.Point(185, 65);
+            this.cmbEntry.Name = "cmbEntry";
+            this.cmbEntry.Size = new System.Drawing.Size(121, 21);
+            this.cmbEntry.TabIndex = 4;
+            // 
+            // cmbOut
+            // 
+            this.cmbOut.FormattingEnabled = true;
+            this.cmbOut.Items.AddRange(new object[] {
+            "Pascal",
+            "atm",
+            "mmHg",
+            "bar",
+            "torr",
+            "psi"});
+            this.cmbOut.Location = new System.Drawing.Point(436, 62);
+            this.cmbOut.Name = "cmbOut";
+            this.cmbOut.Size = new System.Drawing.Size(121, 21);
+            this.cmbOut.TabIndex = 5;
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(185, 143);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(100, 20);
+            this.txtInput.TabIndex = 6;
+            // 
+            // lblDisplay
+            // 
+            this.lblDisplay.AutoSize = true;
+            this.lblDisplay.Location = new System.Drawing.Point(182, 208);
+            this.lblDisplay.Name = "lblDisplay";
+            this.lblDisplay.Size = new System.Drawing.Size(35, 13);
+            this.lblDisplay.TabIndex = 7;
+            this.lblDisplay.Text = "label5";
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(402, 240);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(75, 23);
+            this.btnConvert.TabIndex = 8;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(612, 306);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.lblDisplay);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.cmbOut);
+            this.Controls.Add(this.cmbEntry);
+            this.Controls.Add(this.lblOutputAnswer);
+            this.Controls.Add(this.lblVal);
+            this.Controls.Add(this.lblOut);
+            this.Controls.Add(this.lblEntry);
+            this.Name = "Form1";
+            this.Text = "Pressure Converter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblEntry;
+        private System.Windows.Forms.Label lblOut;
+        private System.Windows.Forms.Label lblVal;
+        private System.Windows.Forms.Label lblOutputAnswer;
+        private System.Windows.Forms.ComboBox cmbEntry;
+        private System.Windows.Forms.ComboBox cmbOut;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label lblDisplay;
+        private System.Windows.Forms.Button btnConvert;
+    }
+}
+
---
+++ Pressure_Converter/Pressure_Converter/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pressure_Converter
+{
+    public partial class Form1 : Form
+    {
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            List<Pressure_Units> lst = new List<Pressure_Units>();
+            lst.Add(new Pressure_Units("Pascal", 1));
+            lst.Add(new Pressure_Units("atm", 2));
+            lst.Add(new Pressure_Units("mmHg", 3));
+            lst.Add(new Pressure_Units("bar", 4));
+            lst.Add(new Pressure_Units("torr", 5));
+            lst.Add(new Pressure_Units("psi", 6));
+            lst.Add(new Pressure_Units("psf", 7));
+
+            cmbEntry.ValueMember = "ID";
+            cmbEntry.DisplayMember = "Name";
+            BindingSource bs1 = new BindingSource();
+            bs1.DataSource = lst;
+            cmbEntry.DataSource = bs1;
+
+            cmbOut.ValueMember = "ID";
+            cmbOut.DisplayMember = "Name";
+            cmbOut.DataSource = lst;
+
+        }
+
+        private double conversion(double value,int from,int to)
+        {
+
+            double[][] pressureTable = {
+                new double[] {1, 0.00000986923266716 ,0.007500616827042 ,0.00001 ,0.007500616827042 ,0.0001450378911491,0.02088545632547  },
+                new double[] {101325 ,1,760 ,1.01325 ,760 ,14.69596432068,2116.218862178  },
+                new double[] {133.3223684211 ,0.001315789473684 ,1,0.001333223684211 ,1,0.01933679515879,2.784498502865  },
+                new double[] {100000 ,0.986923266716 ,750.0616827042 ,1,750.0616827042,14.50378911491,2088.545632547  },
+                new double[] {133.3223684211,0.001315789473684 ,1,0.001333223684211 ,1,0.01933679515879,2.784498502865   },
+                new double[] {6894.75 ,0.0680458919319 ,51.71487786825 ,0.0689475 ,51.71487786825 ,1,144 },
+                new double[] {47.88020833333 ,47.88020833333 ,0.3591310963073 ,0.0004788020833333,0.3591310963073 ,0.006944444444444 ,1}
+            };
+
+
+            return pressureTable[from][to] * value;
+
+
+        }
+
+
+
+        private class Pressure_Units
+        {
+            public string Name { get; set; }
+            public int ID { get; set; }
+
+            public Pressure_Units(string name,int id)
+            {
+                Name = name;
+                ID = id;
+            }
+
+            public static explicit operator Pressure_Units(List<Pressure_Units> v)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            int convertFrom = (int)cmbEntry.SelectedIndex;
+            int convertTo = (int)cmbOut.SelectedIndex;
+
+            string value = txtInput.Text;
+            double test;
+            bool ans=Double.TryParse(value, out test);
+            if (!String.IsNullOrEmpty(value) && Double.TryParse(value,out test))
+            {
+                lblDisplay.Text=conversion(Convert.ToDouble(value), convertFrom, convertTo).ToString();
+            }
+            
+        }
+    }
+}
